0699a796c2397d3e1bf15973b27c31069439e099
Miguel Ruiz Velasco <miguelrvs@fcm.com.mx>
2018-11-04 23:20:45 -0600

    Borrar en Task con dialogo de error

--- VentanaPrueba/src/main/java/fxml/BorrarController.java
+++ VentanaPrueba/src/main/java/fxml/BorrarController.java
@@ -1,25 +1,36 @@
 package fxml;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import org.codehaus.jackson.annotate.JsonAutoDetect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.acerta.abis.dermalog.client.EjecutarWS_Abis_10000;
 
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 
+@Component
 public class BorrarController implements Initializable {
 
 	
 	@FXML private TextField IdTextField;
 	@FXML private Button submitBtn;
 	
-	 private EjecutarWS_Abis_10000 api;
+	@Autowired private EjecutarWS_Abis_10000 api;
 	
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
@@ -29,17 +40,37 @@ public class BorrarController implements Initializable {
     }
 
     @FXML private void handleBorrarAction(){
-    	try {
+
+    	Task<Void> task = new Task<Void>() {
+    	    @Override public Void call() throws IOException {
+    	
 			String id = IdTextField.getText();
 			api.eliminarRegistro(id);
 			System.out.println("Borrar");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+			
+			return null;
+    	    }
+    	    @Override
+    	    protected void succeeded() {
+    	    	Alert alert = new Alert(AlertType.INFORMATION);
+    	    	alert.setTitle("Registro borrado");
+    	    	alert.setHeaderText("Registro borrado");
+    	    	alert.setContentText("Registro borrado");
+    	    	alert.showAndWait();
+    	    }
+    	    @Override
+    	    protected void failed() {
+    	    	super.failed();
+    	    	Throwable e = getException();
+    			e.printStackTrace();
+    			
+    			ErrorMessage.exceptionDialog("Error al Borrar",e);
+    	    }
+    	};
+		
+		new Thread(task).start();
+		
     }
     
-	public void setApi(EjecutarWS_Abis_10000 api) {
-		this.api = api;
-	}
 	
 }
